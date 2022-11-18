7d4b1d601d85ae4f10d48964af64a596f0d70bb0
kevins981 <kevinsong981@gmail.com>
2022-11-18 11:12:44 -0500

    Updated vtune version. Added uarch analysis.

--- exp/run_vtune_exp1_microbench.sh
+++ exp/run_vtune_exp1_microbench.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RESULT_DIR="/ssd1/songxin8/thesis/rocksdb/vtune/exp1_microbench/" 
+RESULT_DIR="/ssd1/songxin8/thesis/rocksdb/vtune/exp1_64GBDIMMs/" 
 DB_DIR="/ssd1/songxin8/thesis/rocksdb/rocksdb/tmp/microbench"
 
 # in seconds
@@ -27,15 +27,21 @@ run_microbench_readrandom () {
   NODE=$2
 
   # setting mem-object-size-min-thres=1MB limit to reduce the size of the vtuen result file on disk.
-  VTUNE_MEMACC_COMMON="/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -collect memory-access \
+  VTUNE_MEMACC_COMMON="/opt/intel/oneapi/vtune/latest/bin64/vtune -collect memory-access \
        -knob sampling-interval=100 -knob analyze-mem-objects=true -knob analyze-openmp=true \
        -knob mem-object-size-min-thres=1048576 \
        -data-limit=10000 -result-dir ${OUTFILE}_memacc\
        --app-working-dir=/ssd1/songxin8/thesis/rocksdb/rocksdb/"
 
-  VTUNE_HOTSPOT_COMMON="/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -collect hotspots \
+  VTUNE_HOTSPOT_COMMON="/opt/intel/oneapi/vtune/latest/bin64/vtune -collect hotspots \
        -data-limit=10000 -result-dir ${OUTFILE}_hotspot \
        --app-working-dir=/ssd1/songxin8/thesis/rocksdb/rocksdb/"
+
+  VTUNE_UARCH_COMMON="/opt/intel/oneapi/vtune/latest/bin64/vtune -collect uarch-exploration \
+       -knob sampling-interval=10 -knob collect-memory-bandwidth=true
+       -data-limit=10000 -result-dir ${OUTFILE}_uarch \
+       --app-working-dir=/ssd1/songxin8/thesis/rocksdb/rocksdb/"
+
 
   pushd ..
 
@@ -77,20 +83,47 @@ run_microbench_readrandom () {
       --level_compaction_dynamic_level_bytes=true --pin_l0_filter_and_index_blocks_in_cache=1 \
       --duration=${DURATION} --threads=32 
   popd
+
+
+  clean_cache
+
+  pushd ..
+
+  ${VTUNE_UARCH_COMMON} -- /usr/bin/numactl --membind=${NODE} --cpunodebind=0 \
+      -- ./db_bench --benchmarks="readrandom,stats" --use_existing_db=1 \
+      --level0_file_num_compaction_trigger=4 --level0_slowdown_writes_trigger=20 \
+      --level0_stop_writes_trigger=30 --max_background_jobs=16 --max_write_buffer_number=8 \
+      --undefok=use_blob_cache,use_shared_block_and_blob_cache,blob_cache_size,blob_cache_numshardbits,prepopulate_blob_cache,multiread_batched,cache_low_pri_pool_ratio,prepopulate_block_cache \
+      --db=${DB_DIR} --wal_dir=${DB_DIR}/WAL_LOG \
+      --num=262144000 --key_size=20 --value_size=400 --block_size=8192 --cache_size=34359738368 \
+      --cache_numshardbits=6 --compression_max_dict_bytes=0 --compression_ratio=0.5 \
+      --bytes_per_sync=1048576 --benchmark_write_rate_limit=0 --write_buffer_size=134217728 \
+      --target_file_size_base=134217728 --max_bytes_for_level_base=1073741824 --verify_checksum=1 \
+      --delete_obsolete_files_period_micros=62914560 --max_bytes_for_level_multiplier=8 \
+      --statistics --histogram=1 --memtablerep=skip_list --bloom_bits=10 --open_files=-1 \
+      --subcompactions=1 --compaction_style=0 --num_levels=8 --min_level_to_compress=-1 \
+      --level_compaction_dynamic_level_bytes=true --pin_l0_filter_and_index_blocks_in_cache=1 \
+      --duration=${DURATION} --threads=32 
+  popd
 }
 
 run_microbench_scan () { 
   OUTFILE=$1 #first argument
   NODE=$2
 
-  VTUNE_MEMACC_COMMON="/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -collect memory-access \
+  VTUNE_MEMACC_COMMON="/opt/intel/oneapi/vtune/latest/bin64/vtune -collect memory-access \
        -knob sampling-interval=100 -knob analyze-mem-objects=true -knob analyze-openmp=true \
        -knob mem-object-size-min-thres=1048576 \
        -data-limit=10000 -result-dir ${OUTFILE}_memacc \
        --app-working-dir=/ssd1/songxin8/thesis/rocksdb/rocksdb/"
 
-  VTUNE_HOTSPOT_COMMON="/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -collect hotspots \
+  VTUNE_HOTSPOT_COMMON="/opt/intel/oneapi/vtune/latest/bin64/vtune -collect hotspots \
        -data-limit=10000 -result-dir ${OUTFILE}_hotspot \
+       --app-working-dir=/ssd1/songxin8/thesis/rocksdb/rocksdb/"
+
+  VTUNE_UARCH_COMMON="/opt/intel/oneapi/vtune/latest/bin64/vtune -collect uarch-exploration \
+       -knob sampling-interval=10 -knob collect-memory-bandwidth=true
+       -data-limit=10000 -result-dir ${OUTFILE}_uarch \
        --app-working-dir=/ssd1/songxin8/thesis/rocksdb/rocksdb/"
 
   pushd ..
@@ -134,6 +167,28 @@ run_microbench_scan () {
       --duration=${DURATION} --threads=32 --seek_nexts=10 --reverse_iterator=false
 
   popd
+
+  clean_cache
+
+  pushd ..
+
+  ${VTUNE_UARCH_COMMON} -- /usr/bin/numactl --membind=${NODE} --cpunodebind=0 \
+      -- ./db_bench --benchmarks="seekrandom,stats" --use_existing_db=1 \
+      --level0_file_num_compaction_trigger=4 --level0_slowdown_writes_trigger=20 \
+      --level0_stop_writes_trigger=30 --max_background_jobs=16 --max_write_buffer_number=8 \
+      --undefok=use_blob_cache,use_shared_block_and_blob_cache,blob_cache_size,blob_cache_numshardbits,prepopulate_blob_cache,multiread_batched,cache_low_pri_pool_ratio,prepopulate_block_cache \
+      --db=${DB_DIR} --wal_dir=${DB_DIR}/WAL_LOG \
+      --num=262144000 --key_size=20 --value_size=400 --block_size=8192 --cache_size=34359738368 \
+      --cache_numshardbits=6 --compression_max_dict_bytes=0 --compression_ratio=0.5 \
+      --bytes_per_sync=1048576 --benchmark_write_rate_limit=0 --write_buffer_size=134217728 \
+      --target_file_size_base=134217728 --max_bytes_for_level_base=1073741824 --verify_checksum=1 \
+      --delete_obsolete_files_period_micros=62914560 --max_bytes_for_level_multiplier=8 \
+      --statistics --histogram=1 --memtablerep=skip_list --bloom_bits=10 --open_files=-1 \
+      --subcompactions=1 --compaction_style=0 --num_levels=8 --min_level_to_compress=-1 \
+      --level_compaction_dynamic_level_bytes=true --pin_l0_filter_and_index_blocks_in_cache=1 \
+      --duration=${DURATION} --threads=32 --seek_nexts=10 --reverse_iterator=false
+
+  popd
 }
 
 
@@ -148,9 +203,9 @@ mkdir -p $RESULT_DIR
 
 clean_cache
 run_microbench_scan "${RESULT_DIR}/scan_allnode0" $workload 0
-clean_cache
-run_microbench_scan "${RESULT_DIR}/scan_allnode1" $workload 1
+#clean_cache
+#run_microbench_scan "${RESULT_DIR}/scan_allnode1" $workload 1
 clean_cache
 run_microbench_readrandom "${RESULT_DIR}/readrandom_allnode0" $workload 0
-clean_cache
-run_microbench_readrandom "${RESULT_DIR}/readrandom_allnode1" $workload 1
+#clean_cache
+#run_microbench_readrandom "${RESULT_DIR}/readrandom_allnode1" $workload 1
--- exp/run_vtune_exp1_zippydb.sh
+++ exp/run_vtune_exp1_zippydb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RESULT_DIR="/ssd1/songxin8/thesis/rocksdb/vtune/exp1_zippydb/" 
+RESULT_DIR="/ssd1/songxin8/thesis/rocksdb/vtune/exp1_64GBDIMMs/" 
 DB_DIR="/ssd1/songxin8/thesis/rocksdb/rocksdb/tmp/zippydb/"
 
 STREADY_STATE_DURATION=900
@@ -31,21 +31,27 @@ run_zippydb () {
   OUTFILE=$1 #first argument
   NODE=$2
 
-  VTUNE_HOTSPOT_COMMON="/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -collect hotspots -start-paused \
+  VTUNE_HOTSPOT_COMMON="/opt/intel/oneapi/vtune/latest/bin64/vtune -collect hotspots -start-paused \
        -data-limit=10000 -result-dir ${OUTFILE}_hotspot \
        --app-working-dir=/ssd1/songxin8/thesis/rocksdb/rocksdb/"
 
   # setting mem-object-size-min-thres=1MB limit to reduce the size of the vtuen result file on disk.
-  VTUNE_MEMACC_COMMON="/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -collect memory-access -start-paused \
+  VTUNE_MEMACC_COMMON="/opt/intel/oneapi/vtune/latest/bin64/vtune -collect memory-access -start-paused \
        -knob sampling-interval=100 -knob analyze-mem-objects=true -knob analyze-openmp=true \
        -knob mem-object-size-min-thres=1048576 \
-       -data-limit=10000 -result-dir ${OUTFILE}_memacc\
+       -data-limit=10000 -result-dir ${OUTFILE}_memacc \
        --app-working-dir=/ssd1/songxin8/thesis/rocksdb/rocksdb/"
+
+  VTUNE_UARCH_COMMON="/opt/intel/oneapi/vtune/latest/bin64/vtune -collect uarch-exploration -start-paused \
+      -knob sampling-interval=10 -knob collect-memory-bandwidth=true
+      -data-limit=10000 -result-dir ${OUTFILE}_uarch \
+      --app-working-dir=/ssd1/songxin8/thesis/rocksdb/rocksdb/"
+
 
   pushd ..
   ####### hotspot analysis
   ${VTUNE_HOTSPOT_COMMON} -- /usr/bin/numactl --membind=${NODE} --cpunodebind=0 \
-      ./db_bench --benchmarks="mixgraph,resetstats,mixgraph,stats" --cache_size=34359738368 \
+      ./db_bench --benchmarks="mixgraph,stats" --cache_size=34359738368 \
       --db=${DB_DIR} --wal_dir=${DB_DIR}/WAL_LOG \
       --cache_numshardbits=6 -use_existing_db=1 -use_direct_io_for_flush_and_compaction=true \
       -use_direct_reads=true -keyrange_dist_a=14.18 -keyrange_dist_b=-2.917 -keyrange_dist_c=0.0164 -keyrange_dist_d=-0.08082 \
@@ -62,7 +68,7 @@ run_zippydb () {
   sleep ${WARMUP_DURATION}
 
   echo "[INFO] Warmup complete. Resuming Vtune profiling."
-  /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command resume -r ${OUTFILE}_hotspot
+  /opt/intel/oneapi/vtune/latest/bin64/vtune -command resume -r ${OUTFILE}_hotspot
 
   echo "[INFO] Waiting for vtune to finish."
   wait $VTUNE_PID
@@ -95,7 +101,38 @@ run_zippydb () {
   sleep ${WARMUP_DURATION}
 
   echo "[INFO] Warmup complete. Resuming Vtune profiling."
-  /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command resume -r ${OUTFILE}_memacc
+  /opt/intel/oneapi/vtune/latest/bin64/vtune -command resume -r ${OUTFILE}_memacc
+
+  echo "[INFO] Waiting for vtune to finish."
+  wait $VTUNE_PID
+
+  popd
+
+  clean_cache
+
+  pushd ..
+
+  ####### uarch analysis
+
+  ${VTUNE_UARCH_COMMON} -- /usr/bin/numactl --membind=${NODE} --cpunodebind=0 \
+      ./db_bench --benchmarks="mixgraph,stats" --cache_size=34359738368 \
+      --db=${DB_DIR} --wal_dir=${DB_DIR}/WAL_LOG \
+      --cache_numshardbits=6 -use_existing_db=1 -use_direct_io_for_flush_and_compaction=true \
+      -use_direct_reads=true -keyrange_dist_a=14.18 -keyrange_dist_b=-2.917 -keyrange_dist_c=0.0164 -keyrange_dist_d=-0.08082 \
+      -keyrange_num=30 -value_k=0.2615 -value_sigma=25.45 -iter_k=2.517 -iter_sigma=14.236 \
+      -mix_get_ratio=0.85 -mix_put_ratio=0.14 -mix_seek_ratio=0.01 \
+      -sine_mix_rate_interval_milliseconds=5000 -sine_a=1000 -sine_b=0.00000073 -sine_d=450000 \
+      --perf_level=2 -num=524288000 -key_size=48 --bloom_bits=10 --write_buffer_size=1073741824 \
+      --max_bytes_for_level_base=10737418240 --max_write_buffer_number=40 \
+      --open_files=-1 --statistics --duration=${TOTAL_DURATION} &
+
+  VTUNE_PID=$!
+
+  echo "[INFO] Sleeping for ${WARMUP_DURATION} to wait for the DRAM cache to warmup. Vtune profiling is not running right now."
+  sleep ${WARMUP_DURATION}
+
+  echo "[INFO] Warmup complete. Resuming Vtune profiling."
+  /opt/intel/oneapi/vtune/latest/bin64/vtune -command resume -r ${OUTFILE}_uarch
 
   echo "[INFO] Waiting for vtune to finish."
   wait $VTUNE_PID
@@ -114,5 +151,5 @@ mkdir -p $RESULT_DIR
 
 clean_cache
 run_zippydb "${RESULT_DIR}/zippydb_allnode0" $workload 0
-clean_cache
-run_zippydb "${RESULT_DIR}/zippydb_allnode1" $workload 1
+#clean_cache
+#run_zippydb "${RESULT_DIR}/zippydb_allnode1" $workload 1
